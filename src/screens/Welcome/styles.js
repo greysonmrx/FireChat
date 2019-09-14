fff65641f3b3c7b6e40b4db24bb74ccc3e318dfe
greysonmrx <greysonmrx@gmail.com>
2019-09-14 14:00:54 -0300

    Criando estilo tela Welcome

---
+++ src/screens/Welcome/styles.js
@@ -0,0 +1,64 @@
+export const Container = { 
+    flex: 1
+}
+
+export const ImageStyle = {
+    width: '100%',
+    height: 250,
+    marginTop: 20
+}
+
+export const ButtonLogin = {
+    width: '100%',
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00B0FF',
+    borderRadius: 8,
+    elevation: 3
+}
+
+export const ButtonRegister = {
+    width: '100%',
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    marginTop: 20,
+    elevation: 3
+}
+
+export const ButtonContainer = {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginTop: 50
+}
+
+export const TextButtonRegister = {
+    fontSize: 16,
+    color: '#404040'
+}
+
+export const TextButtonLogin = {
+    fontSize: 16,
+    color: '#FFFFFF'
+}
+
+export const TextContainer = {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 20,
+    paddingHorizontal: 20
+}
+
+export const TitleStyle = {
+    fontSize: 24
+}
+
+export const TextStyle = {
+    fontSize: 16,
+    color: '#95989A',
+    marginTop: 10,
+    textAlign: 'center'
+}
