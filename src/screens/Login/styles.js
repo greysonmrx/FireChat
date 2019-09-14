24104c9eab01ad43ab41def54c155a8be3f29bcb
greysonmrx <greysonmrx@gmail.com>
2019-09-14 14:59:10 -0300

    Criando estilo tela Login

---
+++ src/screens/Login/styles.js
@@ -0,0 +1,66 @@
+export const Container = { 
+    flex: 1
+}
+
+export const ImageStyle = {
+    width: '100%',
+    height: 250,
+    marginTop: 20
+}
+
+export const TitleStyle = { 
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 20,
+}
+
+export const InputContainer = {
+    width: '100%',
+    paddingHorizontal: 40,
+    marginTop: 20
+}
+
+export const InputEmailStyle = {
+    width: '100%',
+    backgroundColor: '#F1F1F1',
+    height: 40,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    borderRadius: 5
+}
+
+export const ViewInputPasswordStyle = {
+    width: '100%',
+    backgroundColor: '#F1F1F1',
+    height: 40,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    borderRadius: 5,
+    justifyContent: 'space-between',
+    alignItems: 'center'
+}
+
+export const InputPasswordStyle = {
+    flex: 1
+}
+
+export const ButtonContainer = {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginTop: 30
+}
+
+export const Button = {
+    width: '100%',
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00B0FF',
+    borderRadius: 8,
+    elevation: 3
+}
+
+export const TextButton = {
+    fontSize: 16,
+    color: '#FFFFFF'
+}
