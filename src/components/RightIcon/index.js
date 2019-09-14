35370ae6e65f7179639daad30da8c48dd163daf2
greysonmrx <greysonmrx@gmail.com>
2019-09-14 11:52:44 -0300

    Criando o componente RightIcon

---
+++ src/components/RightIcon/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Ionicons } from '@expo/vector-icons';
+
+export default function RightIcon() {
+    return (
+        <Ionicons
+            name="ios-arrow-back"
+            size={ 23 }
+            color="#FFFFFF"
+            style={{ marginRight: 15 }}
+        />
+    );
+}
