b4658046a57548eb8bf131f4e11f6cd05a471300
greysonmrx <greysonmrx@gmail.com>
2019-09-14 17:20:15 -0300

    Corrigindo teclado cobrindo input's - Step1

--- src/screens/Step1/index.js
+++ src/screens/Step1/index.js
@@ -3,7 +3,8 @@ import {
   View, 
   Image,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+  KeyboardAvoidingView
 } from 'react-native';
 
 import { 
@@ -31,53 +32,59 @@ export default function Step1({ navigation }) {
     <View
       style={Container}
     >
-      <Image 
-        source={Step1Image}
-        style={ImageStyle}
-        resizeMode="contain"
-      />
-      <View
-        style={TextContainer}
+      <KeyboardAvoidingView
+        behavior="position"
+        enabled
+        keyboardVerticalOffset={100}
       >
-        <Text style={TitleStyle}>Registrar</Text>
-        <Text
-          style={TextStyle}
-          regular={true}
+        <Image 
+          source={Step1Image}
+          style={ImageStyle}
+          resizeMode="contain"
+        />
+        <View
+          style={TextContainer}
         >
-          O Fire<Text regular style={{ color: "#00B0FF" }}>Chat</Text> vai enviar uma mensagem de SMS para verificar seu número. Insira o código do país e seu número de telefone com DDD.
-        </Text>
-      </View>
-      <View
-        style={InputContainer}
-      >
-        <Entypo 
-          name="plus"
-          size={18}
-          color="#707070"
-        />
-        <TextInput 
-          style={CodeCountryStyle}
-          keyboardType="numeric"
-        />
-        <TextInput 
-          style={PhoneNumberStyle}
-          keyboardType="numeric"
-        />
-      </View>  
-      <View
-        style={ButtonContainer}
-      >
-        <TouchableOpacity
-          style={Button}
-        >
+          <Text style={TitleStyle}>Registrar</Text>
           <Text
+            style={TextStyle}
             regular={true}
-            style={TextButton}
           >
-            Continuar
+            O Fire<Text regular style={{ color: "#00B0FF" }}>Chat</Text> vai enviar uma mensagem de SMS para verificar seu número. Insira o código do país e seu número de telefone com DDD.
           </Text>
-        </TouchableOpacity>  
-      </View>
+        </View>
+        <View
+          style={InputContainer}
+        >
+          <Entypo 
+            name="plus"
+            size={18}
+            color="#707070"
+          />
+          <TextInput 
+            style={CodeCountryStyle}
+            keyboardType="numeric"
+          />
+          <TextInput 
+            style={PhoneNumberStyle}
+            keyboardType="numeric"
+          />
+        </View>  
+        <View
+          style={ButtonContainer}
+        >
+          <TouchableOpacity
+            style={Button}
+          >
+            <Text
+              regular={true}
+              style={TextButton}
+            >
+              Continuar
+            </Text>
+          </TouchableOpacity>  
+        </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
