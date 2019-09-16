160f703eec9f4b7db7b90627251e2c5fad1dd2cf
greysonmrx <greysonmrx@gmail.com>
2019-09-16 13:21:08 -0300

    Adicionando Firebase ao projeto

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Routes from './src/routes';
+import firebase from 'firebase';
+import config from './src/config/config';
+
+console.ignoredYellowBox = ['Warning: Each', "Warning: Failed prop type", 'Setting a timer'];
+console.disableYellowBox = true;
 
 export default function App() {
+    useEffect(() => {
+        firebase.initializeApp(config);
+    }, []);
+
     return <Routes />;
 }
---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBmEcqHsSXPr40aYOa6qoTZpsrqKEUh9EA",
+    authDomain: "chat-teste-130ca.firebaseapp.com",
+    databaseURL: "https://chat-teste-130ca.firebaseio.com",
+    projectId: "chat-teste-130ca",
+    storageBucket: "chat-teste-130ca.appspot.com",
+    messagingSenderId: "1015970207409",
+    appId: "1:1015970207409:web:d394d268107d96d9383490"
+  };
