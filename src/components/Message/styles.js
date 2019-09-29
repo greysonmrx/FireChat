b7b3d8ff0257b6da100ba1ad6e6945e0257c5c4c
greysonmrx <greysonmrx@gmail.com>
2019-09-29 19:39:03 -0300

    Criando estilo do componente Message

---
+++ src/components/Message/styles.js
@@ -0,0 +1,41 @@
+export const Container = {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'flex-start',
+    width: '100%',
+    marginBottom: 10,
+    alignSelf: 'flex-end'
+}
+
+export const ContainerMe = {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    justifyContent: 'flex-end',
+    width: '100%',
+    marginBottom: 10,
+    alignSelf: 'flex-end'
+}
+
+export const MyMessageContainer = {
+    maxWidth: '70%',
+    backgroundColor: '#00B0FF',
+    borderTopRightRadius: 0,
+    borderTopLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginLeft: 10
+}
+
+export const MessageContainer = {
+    maxWidth: '70%',
+    backgroundColor: '#F1F1F1',
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 0,
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginHorizontal: 10
+}
