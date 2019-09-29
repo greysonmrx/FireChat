03bd73105507de218036b2ad6644d9d548e64efb
greysonmrx <greysonmrx@gmail.com>
2019-09-29 19:41:18 -0300

    Modificando estilo do componente HeaderTitle

--- src/components/HeaderTitle/styles.js
+++ src/components/HeaderTitle/styles.js
@@ -2,8 +2,10 @@ export const Container = {
     flexGrow: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    height: '100%'
 } 
 
 export const Title = {
-    fontSize: 18
+    fontSize: 17,
+    color: '#333333'
 }
