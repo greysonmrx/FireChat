5a9c987c8f9865eb88212a474d3817d5cb94ee12
greysonmrx <greysonmrx@gmail.com>
2019-09-29 19:37:36 -0300

    Criando estilo do componente SenderMessage

---
+++ src/components/SenderMessage/styles.js
@@ -0,0 +1,17 @@
+export const buttonContainer = {
+    marginLeft: 5,
+    width: 30,
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+}
+
+export const Container = { 
+    flex: 1,
+    maxHeight: 55,
+    flexDirection: 'row', 
+    elevation: 3, 
+    borderRadius: 100, 
+    paddingHorizontal: 20,
+    marginBottom: 10
+}
