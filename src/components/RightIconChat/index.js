ba99b6e760756218235e640c377a90d1ef3aa3b1
greysonmrx <greysonmrx@gmail.com>
2019-09-29 19:38:34 -0300

    Criando componente RightIconChat

---
+++ src/components/RightIconChat/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { FontAwesome5 } from '@expo/vector-icons';
+
+export default function RightIconChat() {
+    return (
+        <FontAwesome5
+            name="ellipsis-h"
+            size={ 20 }
+            color="#707070"
+            style={{ marginRight: 15 }}
+        />
+    );
+}
