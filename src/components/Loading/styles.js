0637e19c7256b8c36e5eefbd9fb7289fe48ea977
greysonmrx <greysonmrx@gmail.com>
2019-10-02 01:02:49 -0300

    Criando estilo do componente Loading

---
+++ src/components/Loading/styles.js
@@ -0,0 +1,8 @@
+export const Container = {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(255, 255, 255, 0.85)'
+}
