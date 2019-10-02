1cfafb0e6dedfd70cd42da32b2d0e5464f0841fd
greysonmrx <greysonmrx@gmail.com>
2019-10-02 01:05:44 -0300

    Melhorando a UI na tela de Login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -26,95 +26,139 @@ import { Ionicons } from '@expo/vector-icons';
 import LoginImage from '../../../assets/images/loginImage.png';
 import * as firebase from 'firebase';
 import User from '../../User';
+import Loading from '../../components/Loading';
 
 export default function Login({ navigation }) {
-  const [email, setEmail] = useState(undefined);
-  const [password, setPassword] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [passVisibily, setPassVisibility] = useState(false);
   const passwordInput = React.createRef();
+
+  function validForm() {
+    if (email.trim() == '') {
+        alert("Insira um email válido!");
+        return false;
+    } else if (password.trim() == '') {
+        alert("Insira uma senha válida!");
+        return false;
+    }
+
+    return true;
+  }
+
+  function mapError(codeError) {
+    switch (codeError) {
+      case 'auth/invalid-email': {
+        return 'Insira um email válido!';
+      }
+      case 'auth/user-disabled': {
+        return 'Usuário desabilitado!';
+      }
+      case 'auth/user-not-found': {
+        return 'Usuário não encontrado!';
+      }
+      case 'auth/wrong-password': {
+        return 'Senha incorreta!';
+      }
+    }
+
+    return null;
+  }
 
   function focusNextField() {
     passwordInput.current.focus();
   }
 
   async function signIn() {
-    try {
-      const user = await firebase.auth()
-        .signInWithEmailAndPassword(email, password);
-
-      const { user: { uid } } = user;
-      // AsyncStorage.setItem("@User", String(uid));
-      User.uid = uid;
-      navigation.navigate('Home');
-    } catch(err) {
-      console.log(err);
-    }
+    if (validForm()) {
+      try {
+        setLoading(true);
+        const user = await firebase.auth()
+          .signInWithEmailAndPassword(email, password);
+  
+        const { user: { uid } } = user;
+        User.uid = uid;
+        setLoading(false);
+        navigation.navigate('Home');
+      } catch(err) {
+        console.log(err, err.code);
+        let error = mapError(err.code);
+  
+        if (error) {
+          alert(error);
+          setLoading(false);
+        }      
+      }
+    }    
   }
 
   return (
-    <ScrollView
+    <KeyboardAvoidingView
+      behavior="position"
+      enabled
+      keyboardVerticalOffset={50}
       style={Container}
     >
-      <KeyboardAvoidingView
-        behavior="position"
-        enabled
-        keyboardVerticalOffset={100}
-      >
-      <Image 
-        style={ImageStyle}
-        source={LoginImage}
-        resizeMode='contain'
-      />
-      
-      <Text style={TitleStyle}>Olá novamente!</Text>
-      <View
-        style={InputContainer}
-      >
-        <TextInput 
-          style={InputEmailStyle}
-          placeholder="Seu email"
-          autoCapitalize="none"
-          keyboardType="email-address"
-          returnKeyType="next"
-          onSubmitEditing={() => focusNextField()}
-          blurOnSubmit={false}
-          onChangeText={(text) => setEmail(text)}
+      <ScrollView>
+        <Image 
+          style={ImageStyle}
+          source={LoginImage}
+          resizeMode='contain'
         />
+        
+        <Text style={TitleStyle}>Olá novamente!</Text>
         <View
-          style={ViewInputPasswordStyle}
+          style={InputContainer}
         >
           <TextInput 
-            style={InputPasswordStyle}
-            placeholder="Sua senha secreta"
-            secureTextEntry={!passVisibily}
-            returnKeyType="done"
-            ref={passwordInput}
-            onChangeText={(text) => setPassword(text)}
+            style={InputEmailStyle}
+            placeholder="Seu email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            returnKeyType="next"
+            onSubmitEditing={() => focusNextField()}
+            blurOnSubmit={false}
+            onChangeText={(text) => setEmail(text)}
           />
-          <Ionicons 
-            name={passVisibily ? "md-eye-off" : "md-eye"}
-            size={23}
-            color="#404040"
-            onPress={() => setPassVisibility(!passVisibily)}
-          />
-        </View>
-      </View>
-      <View
-        style={ButtonContainer}
-      >
-        <TouchableOpacity
-          style={Button}
-          onPress={() => signIn()}
-        >
-          <Text
-            style={TextButton} 
-            regular={true}
+          <View
+            style={ViewInputPasswordStyle}
           >
-            Entrar
-          </Text>
-        </TouchableOpacity>
-      </View>
-      </KeyboardAvoidingView>
-    </ScrollView>
+            <TextInput 
+              style={InputPasswordStyle}
+              placeholder="Sua senha secreta"
+              secureTextEntry={!passVisibily}
+              returnKeyType="done"
+              ref={passwordInput}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <Ionicons 
+              name={passVisibily ? "md-eye-off" : "md-eye"}
+              size={23}
+              color="#404040"
+              onPress={() => setPassVisibility(!passVisibily)}
+            />
+          </View>
+        </View>
+        <View
+          style={ButtonContainer}
+        >
+          <TouchableOpacity
+            style={Button}
+            onPress={() => signIn()}
+          >
+            <Text
+              style={TextButton} 
+              regular={true}
+            >
+              Entrar
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+      {
+        !!loading && <Loading />
+      }
+    </KeyboardAvoidingView>
   );
 }
